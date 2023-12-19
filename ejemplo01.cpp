74a7b26d6b6e9f8564f0e4fb03b7bd0be7e5c052
cdarboleda <cdarboleda@uce.edu.ec>
2023-12-19 13:51:49 +0000

    Ejemplo 01

--- ejemplo01.cpp
+++ ejemplo01.cpp
@@ -1,3 +1,15 @@
-//
-// Created by gitpod on 12/19/23.
-//
+#include<iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv){
+    MPI_Init(&argc, &argv);
+    int rank, nprocs;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    std::printf("Rank %d of %d procs /n", rank, nprocs);
+
+    MPI_Finalize();
+    return 0;
+}
